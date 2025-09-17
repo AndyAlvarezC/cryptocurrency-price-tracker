fac25c810b16c9d8eff0db24cd093ac7204acfa2
CHAIK70 <andykanka@gmail.com>
2025-09-17 17:39:49 +0200

    feat(form): add form implementation with Zod types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import CryptoSearchForm from "./components/CryptoSearchForm"
 
 function App() {
 
   return (
     <>
-      
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Cryptomonedas</span>
+        </h1>
+
+        <div className="content">
+          <CryptoSearchForm />
+        </div>
+      </div>
     </>
   )
 }
